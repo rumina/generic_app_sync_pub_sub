cc88580506bc8689acc883691930ff1964ad5c42
Rumina Morales <ruminamorales@LT-JLFGC2WRPK.local>
2022-12-14 15:19:00 +0100

    first commit

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,7 +1,4 @@
 const awsmobile = {
-    "aws_appsync_graphqlEndpoint": "https://orzj65l3jvfxpecd2htjukhoku.appsync-api.eu-central-1.amazonaws.com/graphql",
-    "aws_appsync_region": "eu-central-1",
-    "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-c4zqgxrtp5afpnvn67we5g55pa",
+   
     };
 export default awsmobile;
